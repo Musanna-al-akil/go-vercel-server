419b7ca77be62e75edbb83f98c42148a4ed480f5
Musanna Al Akil <musannaal@gmail.com>
2024-04-15 03:26:12 +0600

    remove pointer

--- api/index.go
+++ api/index.go
@@ -18,19 +18,19 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 func handler() http.HandlerFunc {
 	app := fiber.New()
 
-	app.Get("/v1", func(ctx *fiber.Ctx) error {
+	app.Get("/v1", func(ctx fiber.Ctx) error {
 		return ctx.JSON(fiber.Map{
 			"version": "v1",
 		})
 	})
 
-	app.Get("/v2", func(ctx *fiber.Ctx) error {
+	app.Get("/v2", func(ctx fiber.Ctx) error {
 		return ctx.JSON(fiber.Map{
 			"version": "v2",
 		})
 	})
 
-	app.Get("/", func(ctx *fiber.Ctx) error {
+	app.Get("/", func(ctx fiber.Ctx) error {
 		return ctx.JSON(fiber.Map{
 			"uri":  ctx.Request().URI().String(),
 			"path": ctx.Path(),
