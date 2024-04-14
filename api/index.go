154947db8a6e58d13c99acd156ec3dc428d92bb7
Musanna Al Akil <musannaal@gmail.com>
2024-04-15 03:15:57 +0600

    change to v2

--- api/index.go
+++ api/index.go
@@ -1,8 +1,8 @@
 package handler
 
 import (
-	"github.com/gofiber/fiber/v3/middleware/adaptor"
-	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v2/middleware/adaptor"
+	"github.com/gofiber/fiber/v2"
 	"net/http"
 )
 
